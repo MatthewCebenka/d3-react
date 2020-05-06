6f8e646f720a0f0bd98d7bdd8996a7880f6b20f4
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-06 14:46:19 -0400

    version one

--- d3/src/App.js
+++ d3/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import { select } from "d3";
+import "./App.css";
+
+const data = [24, 30, 45, 60, 20];
 
 function App() {
+  const svgRef = useRef();
+  console.log(svgRef);
+
+  useEffect(() => {
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    svg
+    .selectAll("circle")
+    .data(data)
+    .join(
+      enter => 
+        enter
+          .append('circle')
+          .attr('r', value => value)
+          .attr('cx', value => value * 2)
+          .attr('cy', value => value * 2)
+          .attr('stroke', 'blue'),
+      update => update.attr('class', 'update'),
+      exit => exit.remove()
+    );
+    console.log(svg.selectAll("circle").data(data));
+  }, []);
   return (
-<div>
-    <h1>Hello World</h1>
-</div>
+    <div>
+      <h1>Hello World</h1>
+      <svg ref={svgRef}></svg>
+    </div>
   );
 }
 
